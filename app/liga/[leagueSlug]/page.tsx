8c7d06861af79e8f2ac117ebcfe0cc3622f56401
easyleasyse-droid <easyleasy.se@gmail.com>
2026-08-19 14:09:10 +0200

    nytt försök

--- app/liga/[leagueSlug]/page.tsx
+++ app/liga/[leagueSlug]/page.tsx
@@ -1,11 +1,10 @@
 import LeagueClient from "./LeagueClient";
-import { LEAGUES_DATA } from "../../../leagues";
+import { LEAGUES_DATA } from "../../leagues";
 import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: Promise<{ leagueSlug: string }> }) {
   const { leagueSlug } = await params;
 
-  // Kontrollera att ligan finns innan vi renderar klient-komponenten
   if (!LEAGUES_DATA[leagueSlug]) {
     notFound();
   }
